1dcb17ee8973d0f468a0d9958af619d7d7f0de6a
SudhaAmarnath <sudha04.a@gmail.com>
2018-12-02 21:31:17 -0800

    Reload browser window after user logs in

--- UserInterface/src/user/Login.js
+++ UserInterface/src/user/Login.js
@@ -79,6 +79,7 @@ class LoginForm extends Component {
             localStorage.setItem(ACCESS_TOKEN, response.accessToken);
             Alert.success("You're successfully logged in!");
             this.props.history.push("/");
+            window.location.reload()
         }).catch(error => {
             Alert.error((error && error.message) || 'Something went wrong. Please try again!');
         });
